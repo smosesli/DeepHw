70f79a4d2cc0517ebc69690b48e23b9cab4f1b78
baklawe <baklawe@yahoo.com>
2018-11-07 18:53:32 +0200

    Finnised part2

--- answers.py
+++ answers.py
@@ -12,14 +12,11 @@ part2_q1 = r"""
 **Your answer:**
 
 
-Write your answer using **markdown** and $\LaTeX$:
-```python
-# A code block
-a = 2
-```
-An equation: $e^{i\pi} -1 = 0$
+From our experiments we can see that increasing k usually increase the generalization of our model up to some point. 
+If we look at to extremum we can see that if we take k as one we have a greater risk for error with only one neighbor. 
+On the other side if we take k to be large the we will get that the decision will be made from the majority of some 
+class and not the nearest neighbors.
 
-"""
 # ==============
 
 # ==============
